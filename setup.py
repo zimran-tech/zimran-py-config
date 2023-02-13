1f050f75ab836a7ee1b4b300a8f9b562eb69f10e
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-13 14:29:19 +0600

    add publish workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import find_namespace_packages, setup
+# from setuptools import find_namespace_packages, setup
+#
+# setup(
+#     name='zimran-config',
+#     version='1.0.0',
+#     packages=find_namespace_packages(include=['zimran.*']),
+#     install_requires=['pydantic'],
+#     python_requires='>=3.10',
+#     zip_file=False,
+# )
 
-setup(
-    name='zimran-config',
-    version='1.0.0',
-    packages=find_namespace_packages(include=['zimran.*']),
-    install_requires=['pydantic'],
-    python_requires='>=3.10',
-    zip_file=False,
-)
+from os import environ
+
+print(environ)  # noqa
