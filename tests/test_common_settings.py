ec10d82f0dc6e8843fc115e4710947748288bf08
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-13 12:19:29 +0600

    initial commit

---
+++ tests/test_common_settings.py
@@ -0,0 +1,48 @@
+from os import environ
+
+import pytest
+from pydantic import ValidationError
+from pytest_mock import MockerFixture
+
+from zimran.settings import CommonSettings, Environment
+
+SENTRY_DSN = 'https://public@sentry.example.com/1'
+
+
+def test_sentry_dsn_ok(mocker: MockerFixture) -> None:
+    mocker.patch.dict(environ, {'SENTRY_DSN': SENTRY_DSN})
+    settings = CommonSettings()
+    assert settings.sentry_dsn == SENTRY_DSN
+
+
+def test_sentry_dsn_fail() -> None:
+    with pytest.raises(ValidationError):
+        CommonSettings()
+
+
+def test_debug_false(mocker: MockerFixture) -> None:
+    mocker.patch.dict(environ, {'SENTRY_DSN': SENTRY_DSN})
+    settings = CommonSettings()
+    assert settings.debug is False
+
+
+def test_debug_true(mocker: MockerFixture) -> None:
+    mocker.patch.dict(environ, {'SENTRY_DSN': SENTRY_DSN, 'DEBUG': 'true'})
+    settings = CommonSettings()
+    assert settings.debug is True
+
+
+@pytest.mark.parametrize('environment', [
+    Environment.DEVELOPMENT, Environment.STAGING, Environment.PRODUCTION,
+])
+def test_environment_ok(mocker: MockerFixture, environment: Environment) -> None:
+    mocker.patch.dict(environ, {'SENTRY_DSN': SENTRY_DSN, 'ENVIRONMENT': environment})
+    settings = CommonSettings()
+    assert settings.environment == environment
+
+
+def test_environment_fail(mocker: MockerFixture) -> None:
+    mocker.patch.dict(environ, {'SENTRY_DSN': SENTRY_DSN, 'ENVIRONMENT': 'wrong_environment'})
+
+    with pytest.raises(ValidationError):
+        CommonSettings()
---
+++ zimran/settings/__init__.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+from pydantic import AnyUrl, BaseSettings
+
+
+class Environment(str, Enum):
+    DEVELOPMENT = 'development'
+    STAGING = 'staging'
+    PRODUCTION = 'production'
+
+
+class CommonSettings(BaseSettings):
+    debug: bool = False
+    environment: Environment = Environment.DEVELOPMENT
+    sentry_dsn: AnyUrl
