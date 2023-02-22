731c45ce14c9a2fa529435d9b5e9288bed400f52
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-22 15:13:16 +0600

    make sentry_dsn optional

--- zimran/config/__init__.py
+++ zimran/config/__init__.py
@@ -12,4 +12,4 @@ class Environment(str, Enum):
 class CommonSettings(BaseSettings):
     debug: bool = False
     environment: Environment = Environment.DEVELOPMENT
-    sentry_dsn: AnyUrl
+    sentry_dsn: AnyUrl = None
